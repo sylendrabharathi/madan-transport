f2e84016ce5f48ed446894e84bbcc8ebcda8e1d4
sylendrabharathi <sylendrabharathi@gmail.com>
2020-08-29 16:45:34 +0530

    change button

--- madhan-lorry-owner/src/app/my-bookings/my-bookings.module.ts
+++ madhan-lorry-owner/src/app/my-bookings/my-bookings.module.ts
@@ -6,11 +6,13 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MyBookingsComponent } from './my-bookings/my-bookings.component';
 import { MyBookingsApiService } from './services/api/my-bookings-api.service';
+import { BookingComponent } from './booking/booking.component';
 
 
 @NgModule({
   declarations: [
-    MyBookingsComponent
+    MyBookingsComponent,
+    BookingComponent
   ],
   imports: [
     CommonModule,
