f0cf8e251eec8fcbea365a25b92b111b589b4328
sylendrabharathi <sylendrabharathi@gmail.com>
2020-10-18 19:15:36 +0530

    change logic for language

--- madhan-lorry-owner/src/app/app.component.ts
+++ madhan-lorry-owner/src/app/app.component.ts
@@ -50,38 +50,40 @@ export class AppComponent implements OnInit {
 
 
   formMenuList() {
-    if (this.lsService.getMyLanguage()) {
-      this.translate.use(this.lsService.getMyLanguage());
-      setTimeout(() => {
-        console.log(this.translate.instant('menuTitles.Home'));
-        var Enquires = this.translate.get('menuTitles.Booking Enquires');
-        const menuIcons = ['home', 'bookmarks-outline', 'book-outline', 'bus-outline', 'man-outline', 'people-outline',
-          'map-outline', 'settings-outline', 'person-outline'];
-        const menuTitles = [this.translate.instant('menuTitles.Home'),
-        this.translate.instant('menuTitles.Booking Enquires'),
-        this.translate.instant('menuTitles.My Bookings'),
-        this.translate.instant('menuTitles.Manage Vehicle'),
-        this.translate.instant('menuTitles.Manage Driver'),
-        this.translate.instant('menuTitles.Driver In Out'),
-        this.translate.instant('menuTitles.Track'),
-        this.translate.instant('menuTitles.Settings'),
-        this.translate.instant('menuTitles.My Profile')];
-        const menuLinks = ['home', 'booking-enquires', 'my-bookings', 'manage-vehicle', 'manage-driver', 'driver-in-out',
-          'track', 'settings', 'my-profile'];
-        const menus = [];
-        for (let i = 0; i < menuTitles.length; i++) {
-          const menuListItem = {
-            icon: menuIcons[i],
-            title: menuTitles[i],
-            url: menuLinks[i]
-          };
-          menus.push(menuListItem);
-        }
-        this.menus = menus;
-
-      }, 200)
-
+    let myLanguage = this.lsService.getMyLanguage();
+    if (!myLanguage) {
+      myLanguage = 'en';
     }
+    this.translate.use(myLanguage);
+    setTimeout(() => {
+      console.log(this.translate.instant('menuTitles.Home'));
+      var Enquires = this.translate.get('menuTitles.Booking Enquires');
+      const menuIcons = ['home', 'bookmarks-outline', 'book-outline', 'bus-outline', 'man-outline', 'people-outline',
+        'map-outline', 'settings-outline', 'person-outline'];
+      const menuTitles = [this.translate.instant('menuTitles.Home'),
+      this.translate.instant('menuTitles.Booking Enquires'),
+      this.translate.instant('menuTitles.My Bookings'),
+      this.translate.instant('menuTitles.Manage Vehicle'),
+      this.translate.instant('menuTitles.Manage Driver'),
+      this.translate.instant('menuTitles.Driver In Out'),
+      this.translate.instant('menuTitles.Track'),
+      this.translate.instant('menuTitles.Settings'),
+      this.translate.instant('menuTitles.My Profile')];
+      const menuLinks = ['home', 'booking-enquires', 'my-bookings', 'manage-vehicle', 'manage-driver', 'driver-in-out',
+        'track', 'settings', 'my-profile'];
+      const menus = [];
+      for (let i = 0; i < menuTitles.length; i++) {
+        const menuListItem = {
+          icon: menuIcons[i],
+          title: menuTitles[i],
+          url: menuLinks[i]
+        };
+        menus.push(menuListItem);
+      }
+      this.menus = menus;
+
+    }, 200)
+
 
 
   }
